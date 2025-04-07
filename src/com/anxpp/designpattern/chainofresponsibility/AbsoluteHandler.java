45a011794a9b6b955ecd66cbbeb0db693b8c4871
hp <krzydeix@outlook.com>
2025-04-08 01:40:15 +0800

    [edit]职责链模式重命名部分变量，补充注释，提高代码可读性

---
+++ src/com/anxpp/designpattern/chainofresponsibility/AbsoluteHandler.java
@@ -0,0 +1,22 @@
+package com.anxpp.designpattern.chainofresponsibility;
+
+// 第一个具体处理者，处理小于0的情况
+public class AbsoluteHandler implements Handler {
+    private Handler next;
+
+    @Override
+    public int handleRequest(int n) {
+        if (n < 0) {
+            return -n;
+        }
+        if (next == null) {
+            throw new NullPointerException("next 不能为空");
+        }
+        return next.handleRequest(n);
+    }
+
+    @Override
+    public void setNextHandler(Handler next) {
+        this.next = next;
+    }
+}
--- src/com/anxpp/designpattern/chainofresponsibility/TestUse.java
+++ src/com/anxpp/designpattern/chainofresponsibility/TestUse.java
@@ -1,15 +1,21 @@
 package com.anxpp.designpattern.chainofresponsibility;
 
 public class TestUse {
-    public static void main(String args[]) {
-        Handler h1, h2, h3;
-        h1 = new Handler1();
-        h2 = new Handler2();
-        h3 = new Handler3();
-        h1.setNextHandler(h2);
-        h2.setNextHandler(h3);
-        System.out.println(h1.handleRequest(-1));
-        System.out.println(h1.handleRequest(5));
-        System.out.println(h1.handleRequest(9999));
+    public static void main(String[] args) {
+        // 初始化处理类
+        Handler absoluteHandler = new AbsoluteHandler();
+        Handler squareHandler = new SquareHandler();
+        Handler nativeHandler = new NativeHandler();
+
+        // 设置处理类的先后关系，即完成职责链的初始化
+        absoluteHandler.setNextHandler(squareHandler);
+        squareHandler.setNextHandler(nativeHandler);
+
+        // 对于 n = -1，返回其绝对值（n < 0）
+        System.out.println(absoluteHandler.handleRequest(-1));
+        // 对于 n = 5，返回其平方（n >= 0 && n <= 10）
+        System.out.println(absoluteHandler.handleRequest(5));
+        // 对于 n = 9999，返回其原值（n > 0 && n < Integer.MAX_VALUE）
+        System.out.println(absoluteHandler.handleRequest(9999));
     }
 }
