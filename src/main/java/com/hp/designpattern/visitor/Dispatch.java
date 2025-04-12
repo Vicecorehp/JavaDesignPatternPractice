d6f45f786d48582d7e356f57201f00a8db3583aa
hp <krzydeix@outlook.com>
2025-04-12 12:46:29 +0800

    [edit]visitor模块代码完善细节

--- src/main/java/com/hp/designpattern/visitor/Dispatch.java
+++ src/main/java/com/hp/designpattern/visitor/Dispatch.java
@@ -4,18 +4,26 @@ package com.hp.designpattern.visitor;
 // 结果会输出：Collection
 // 所以重载的分派是根据静态类型进行的
 public class Dispatch {
-    void print(FatherClass c) {
-        System.out.print("父类");
+    void print(FatherClass ignored) {
+        System.out.println("父类");
     }
 
-    void print(ChildClass c) {
-        System.out.print("子类");
+    void print(ChildClass ignored) {
+        System.out.println("子类");
     }
 
     public static void main(String[] args) {
         FatherClass child = new ChildClass();
         new Dispatch().print(child);
+
+        FatherClass father = new FatherClass();
+        new Dispatch().print(father);
+
+        ChildClass childAnother = new ChildClass();
+        new Dispatch().print(childAnother);
+
         child.print();
+        father.print();
     }
 }
 
@@ -26,6 +34,7 @@ class FatherClass {
 }
 
 class ChildClass extends FatherClass {
+    @Override
     void print() {
         System.out.println("子类");
     }
